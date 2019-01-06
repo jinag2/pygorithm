fa0369b77ef648a5ecef47d89c648633e13df138
jinag2 <jinag2@gmail.com>
2019-01-06 23:12:32 +0800

    interpolation_search()

--- search.py
+++ search.py
@@ -1,4 +1,4 @@
-def binary_search(data, value):
+def binary_search(data, key):
     """
     >>> binary_search([], 23)
     False
@@ -10,16 +10,46 @@ def binary_search(data, value):
     if len(data) == 0:
         return False
 
-    left = 0
-    right = len(data) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if value == data[mid]:
+    low = 0
+    high = len(data) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if key == data[mid]:
             return True
-        elif value < data[mid]:
-            right = mid - 1
+        elif key < data[mid]:
+            high = mid - 1
         else:
-            left = mid + 1
+            low = mid + 1
+
+    return False
+
+
+def interpolation_search(data, key):
+    """
+    >>> interpolation_search([], 23)
+    False
+    >>> interpolation_search([3, 10, 12, 18, 23, 35, 42, 51, 62, 79], 23)
+    True
+    >>> interpolation_search([3, 10, 12, 18, 23, 35, 42, 51, 62, 79], 47)
+    False
+    """
+    if len(data) == 0:
+        return False
+
+    low = 0
+    high = len(data) - 1
+    while low <= high:
+        mid = int(low + ((key - data[low]) / (data[high] - data[low])) * (high - low))
+        if mid < low or mid > high:
+            break
+        if key == data[mid]:
+            return True
+        elif key < data[mid]:
+            high = mid - 1
+        elif key > data[mid]:
+            low = mid + 1
+        else:
+            return False
 
     return False
 
