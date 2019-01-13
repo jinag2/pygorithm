7c6733ba638df9864c5b1c1874c1fdfb7b2a6efb
jinag2 <jinag2@gmail.com>
2019-01-13 22:35:19 +0800

    SingleLink

--- link_list.py
+++ link_list.py
@@ -1,3 +1,6 @@
+import io
+
+
 class _Node:
 
     def __init__(self, value):
@@ -18,13 +21,13 @@ class _Node:
 
     @next.setter
     def next(self, value):
-        if isinstance(value, _Node):
+        if isinstance(value, _Node) or not value:
             self._next = value
         else:
             raise ValueError
 
     def __str__(self):
-        return "('data': {_data}, 'next': {_next})".format(**self.__dict__)
+        return "({_data})".format(**self.__dict__)
 
 
 class SingleLink:
@@ -42,6 +45,26 @@ class SingleLink:
                     return p
                 else:
                     p = p.next
+
+    def __str__(self):
+        output = io.StringIO()
+        p = self._head
+        while p:
+            if p.next:
+                output.write("{0}, ".format(p))
+            else:
+                output.write("{0}".format(p))
+            p = p.next
+        return output.getvalue()
+
+    def _find_node(self, data):
+        p = self._head
+        while p:
+            if p.data == data:
+                return p
+            else:
+                p = p.next
+        return p
 
     def head(self):
         if not self._head:
@@ -63,7 +86,45 @@ class SingleLink:
         else:
             tail_node.next = _Node(data)
 
+    def insert_after(self, target, data):
+        new_node = _Node(data)
+        node = self._find_node(target)
+        if node:
+            if node.next:
+                new_node.next = node.next
+            node.next = new_node
+        else:
+            if self._head:
+                tail_node = self._tail()
+                tail_node.next = new_node
+            else:
+                self._head = new_node
 
-link = SingleLink()
-link.add("Head")
-node1 = link.head()
+    def delete(self, data):
+        if not self._head:
+            return False
+
+        if self._head.data == data:
+            self._head = self._head.next
+            return True
+        else:
+            pre = self._head
+            current = pre.next
+            while current:
+                if current.data == data:
+                    pre.next = current.next
+                    return True
+                else:
+                    pre = current
+                    current = current.next
+        return False
+
+    def reverse(self):
+        p = self._head
+        new_link = None
+        while p:
+            node = p
+            p = p.next
+            node.next = new_link
+            new_link = node
+        self._head = new_link
